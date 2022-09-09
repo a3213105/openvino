76a20b92f0df91a4a681292042d75b5ecb728980
a3213105 <sheng.gui@intel.com>
2022-09-09 21:08:07 +0800

    temporary avoid bool checkw

--- src/core/src/op/util/binary_elementwise_arithmetic.cpp
+++ src/core/src/op/util/binary_elementwise_arithmetic.cpp
@@ -29,11 +29,11 @@ void ov::op::util::BinaryElementwiseArithmetic::validate_and_infer_elementwise_a
     element::Type& args_et = std::get<0>(args_et_pshape);
     PartialShape& args_pshape = std::get<1>(args_et_pshape);
 
-    NODE_VALIDATION_CHECK(this,
-                          args_et.is_dynamic() || args_et != element::boolean,
-                          "Arguments cannot have boolean element type (argument element type: ",
-                          args_et,
-                          ").");
+//    NODE_VALIDATION_CHECK(this,
+//                         args_et.is_dynamic() || args_et != element::boolean,
+//                          "Arguments cannot have boolean element type (argument element type: ",
+//                          args_et,
+//                          ").");
 
     set_output_type(0, args_et, args_pshape);
 }
