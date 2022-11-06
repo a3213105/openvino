52ccfa373218d3ad6a9f845761aba20d6edd5b8c
a3213105 <sheng.gui@intel.com>
2022-11-06 19:17:37 +0800

    fix some avx-sse penalty

--- src/plugins/intel_cpu/src/node.h
+++ src/plugins/intel_cpu/src/node.h
@@ -584,6 +584,14 @@ protected:
         this->type = type;
     }
 
+    virtual std::string getDNNlString() {
+        if (prim) {
+            return (*prim).impl_info();
+        }
+
+        return "";
+    }
+
     virtual size_t getMaxBatch() const;
 
 
